b4264948757265868db4505ac45a449b8a2bdb71
AlightKnight343 <thegodofgreatness343@gmail.com>
2022-02-06 19:08:29 +0530

    delay

--- routes/downloader.js
+++ routes/downloader.js
@@ -23,13 +23,14 @@ const spotifyApi = new SpotifyWebApi({
 })
 
 router.post('/', async (req, res) => {
+    const delay = ms => new Promise(res => setTimeout(res, ms));
 
     const credsResponse = await spotifyApi.clientCredentialsGrant()
     spotifyApi.setAccessToken(credsResponse.body['access_token'])
     const spotifyToYoutube = SpotifyToYoutube(spotifyApi)
     try {
         const trackOrPlaylist = await spotifyToYT.isTrackOrPlaylist(req.body.link)
-        
+
         if (trackOrPlaylist == "track") {
             let trackDetails = await spotifyToYT.trackGet(req.body.link)
 
@@ -43,7 +44,7 @@ router.post('/', async (req, res) => {
                     return trackDetails.info[0].title + songId + ".mp3"
                 }
             })
-            
+
 
 
             const parsed = spotifyUri.parse(req.body.link);
@@ -55,7 +56,10 @@ router.post('/', async (req, res) => {
                 songId: songId,
                 path: `downloaded/songs/${trackDetails.info[0].title}${songId}.mp3`
             })
-            await song.save().then(() => res.render("song", {href: `/song/${songId}`}))
+            await song.save().then(async () => {
+                await delay(3000) 
+                res.render("song", { href: `/song/${songId}` })
+            })
         }
 
 
@@ -64,7 +68,7 @@ router.post('/', async (req, res) => {
             let playlistSongs = playlist.songs
             const playlistId = nanoid()
             let dir = "./downloaded/playlists/" + playlist.info.name + playlistId;
-            
+
             if (!fs.existsSync(dir)) {
                 fs.mkdirSync(dir);
             }
@@ -77,7 +81,7 @@ router.post('/', async (req, res) => {
                     return videotitle + songId + ".mp3"
                 }
             })
-            playlistSongs.forEach( async songs => {
+            playlistSongs.forEach(async songs => {
                 const songid = getVideoId(songs).id
                 console.log(songid);
                 await downloader.download(songid).then(console.log('Playlist Downloaded'))
@@ -89,24 +93,26 @@ router.post('/', async (req, res) => {
                 playlistId: playlistId,
                 path: `downloaded/playlists/${playlist.info.name}${playlistId}`
             })
-            await playlistSchema.save().then(() => res.render("playlist", {href: `/playlist/${playlistId}`}))
+            await playlistSchema.save().then(async() => {
+                await delay(3000) 
+                res.render("playlist", { href: `/playlist/${playlistId}` })
+            })
 
-            const delay = ms => new Promise(res => setTimeout(res, ms));
 
             const compressFunction = async () => {
                 await delay(4000)
-                const sourceDir =`./downloaded/playlists/${playlist.info.name}${playlistId}`;
+                const sourceDir = `./downloaded/playlists/${playlist.info.name}${playlistId}`;
                 const destinationDir = `./downloaded/playlists/${playlist.info.name}${playlistId}.tar`;
                 await compressing.tar.compressDir(sourceDir, destinationDir)
-                .then(console.log("compressed"))
-                .catch(err => console.log(err));
+                    .then(console.log("compressed"))
+                    .catch(err => console.log(err));
             }
             compressFunction()
 
+        }
     }
-}
     catch (e) {
-        console.log(e)
+        res.send(e)
     }
 
 })
