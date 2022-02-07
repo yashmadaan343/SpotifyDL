c91c7d50cfddf6a3967572b3afab7f470497d02e
AlightKnight343 <thegodofgreatness343@gmail.com>
2022-02-07 13:32:17 +0530

    port kek

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use("/playlist", playlistRoute)
   
 
 const PORT = process.env.PORT || 5000
-app.listen(5000, ()=>{
+app.listen(PORT, ()=>{
   console.log(`Server started on port ${PORT}`)
 })
 
